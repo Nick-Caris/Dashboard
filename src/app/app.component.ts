eac6c8e876d73e0dd27cca48984631a03c2f347b
Nick Caris <nickcaris97@gmail.com>
2019-07-22 23:13:20 +0200

    First API call settup for weather block

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Dashboard';
+  weather: any[] = [];
+
+  constructor(private http: HttpClient) {
+    this.getWeather();
+  }
+
+
+  private getWeather() {
+    console.log('start func');
+    this.http.get('http://weerlive.nl/api/json-data-10min.php?key=51a306b528&locatie=Nijmegen').pipe(map(data => {
+      console.log(data);
+      this.weather.push({
+        key: 'plaats',
+        value: data
+      });
+      // this.weather['temp'] = data['lifeweer'];
+      console.log(this.weather);
+    })).subscribe();
+
+  }
+
+
 }
